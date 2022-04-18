d42868f26dd5c5fd75799b8536df461945f4af23
Jackboby <101992698+Jackboby@users.noreply.github.com>
2022-04-17 23:22:57 -0600

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var a = document.getElementById("a").innerHTML = "hey ";
+var b = document.getElementById("b").innerHTML = "cars ";
+var c = document.getElementById("c").innerHTML = "lol ";
+//typewritter effect 
+document.getElementById("main").innerHTML = "";
+setTimeout(() => {
+document.getElementById("main").innerHTML = (a);
+}, 100);
+setTimeout(() => {
+document.getElementById("main").innerHTML = (a + b);
+}, 300);
+setTimeout(() => {
+document.getElementById("main").innerHTML = (a + b + c);
+}, 500);
